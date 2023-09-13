28286d1774a96fc1643b038d6ec6c63df831bb87
Carlos Gutiérrez <ceguro10@gmail.com>
2023-09-12 22:54:18 -0300

    feat: use dependency injection with a constructor

--- src/main/java/com/kafkaApplication/controller/KafkaController.java
+++ src/main/java/com/kafkaApplication/controller/KafkaController.java
@@ -4,11 +4,9 @@ import com.kafkaApplication.model.MessageRequest;
 import com.kafkaApplication.service.KafkaProducerService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,7 +15,6 @@ public class KafkaController {
     private final KafkaProducerService kafkaProducerService;
     private static final Logger log = LogManager.getLogger(KafkaController.class);
 
-    @Autowired
     public KafkaController(KafkaProducerService kafkaProducerService) {
         this.kafkaProducerService = kafkaProducerService;
     }
